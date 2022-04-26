ec77d788d320f3bca3958281b2f3a9778fa6be79
Osazee Oghagbon <63594399+osazeejedi@users.noreply.github.com>
2022-04-26 10:26:32 +0100

    Initialize simple-marketplace-royalties

---
+++ README.md
@@ -0,0 +1,49 @@
+# 🏯 Simple Marketplace w/ Royalties
+
+![image](https://scaffold-eth-readme-images.s3.amazonaws.com/Screen+Shot+2022-04-13+at+11.50.39+AM.png)
+
+This Marketplace template features the following:
+  - `createListing`
+  - `buy`
+  - `bid`
+  - `setNFTCollectionRoyalty`
+
+# 🏄‍♂️ Quick Start
+
+Prerequisites: [Node (v16 LTS)](https://nodejs.org/en/download/) plus [Yarn](https://classic.yarnpkg.com/en/docs/install/) and [Git](https://git-scm.com/downloads)
+
+> clone/fork 🏗 scaffold-eth:
+
+```bash
+git clone https://github.com/scaffold-eth/scaffold-eth.git
+```
+
+> install and start your 👷‍ Hardhat chain:
+
+```bash
+cd scaffold-eth
+yarn install
+yarn chain
+```
+
+> in a second terminal window, start your 📱 frontend:
+
+```bash
+cd scaffold-eth
+yarn start
+```
+
+> in a third terminal window, 🛰 deploy your contract:
+
+```bash
+cd scaffold-eth
+yarn deploy
+```
+
+🔏 Edit your smart contract `YourContract.sol` in `packages/hardhat/contracts`
+
+📝 Edit your frontend `App.jsx` in `packages/react-app/src`
+
+💼 Edit your deployment scripts in `packages/hardhat/deploy`
+
+📱 Open http://localhost:3000 to see the app
---
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -0,0 +1,104 @@
+// deploy/00_deploy_your_contract.js
+
+const { ethers } = require("hardhat");
+
+const localChainId = "31337";
+
+// const sleep = (ms) =>
+//   new Promise((r) =>
+//     setTimeout(() => {
+//       console.log(`waited for ${(ms / 1000).toFixed(3)} seconds`);
+//       r();
+//     }, ms)
+//   );
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
+
+  await deploy("MockERC721", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    // args: [ "Hello", ethers.utils.parseEther("1.5") ],
+    log: true,
+    waitConfirmations: 5,
+  });
+  await deploy("MockERC20", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    // args: [ "Hello", ethers.utils.parseEther("1.5") ],
+    log: true,
+    waitConfirmations: 5,
+  });
+  await deploy("Marketplace", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    // args: [ "Hello", ethers.utils.parseEther("1.5") ],
+    log: true,
+    waitConfirmations: 5,
+  });
+
+  // Getting a previously deployed contract
+  const MockERC20 = await ethers.getContract("MockERC20", deployer);
+  await MockERC20.mint('0x21aDafAA34d250a4fa0f8A4d2E2424ABa0cEE563', 1000000)
+  await MockERC20.mint('0x980B7aD36055942B2074C2804c6C0BfA548ac9E1', 1000000)
+  await MockERC20.mint('0x8A759Cd31ac3EC1aCA025d8193a7877a07a56837', 1000000)
+  const MockERC721 = await ethers.getContract("MockERC721", deployer);
+  const Marketplace = await ethers.getContract("Marketplace", deployer);
+  await Marketplace.setNFTCollectionRoyalty(
+        MockERC721.address,
+        "YourCollectible",
+        '0x21aDafAA34d250a4fa0f8A4d2E2424ABa0cEE563',
+        1000
+    );
+
+  /*
+    To take ownership of yourContract using the ownable library uncomment next line and add the
+    address you want to be the owner.
+    // await yourContract.transferOwnership(YOUR_ADDRESS_HERE);
+
+    //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
+  */
+
+  /*
+  //If you want to send value to an address from the deployer
+  const deployerWallet = ethers.provider.getSigner()
+  await deployerWallet.sendTransaction({
+    to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
+    value: ethers.utils.parseEther("0.001")
+  })
+  */
+
+  /*
+  //If you want to send some ETH to a contract on deploy (make your constructor payable!)
+  const yourContract = await deploy("YourContract", [], {
+  value: ethers.utils.parseEther("0.05")
+  });
+  */
+
+  /*
+  //If you want to link a library into your contract:
+  // reference: https://github.com/austintgriffith/scaffold-eth/blob/using-libraries-example/packages/hardhat/scripts/deploy.js#L19
+  const yourContract = await deploy("YourContract", [], {}, {
+   LibraryName: **LibraryAddress**
+  });
+  */
+
+  // Verify from the command line by running `yarn verify`
+
+  // You can also Verify your contracts with Etherscan here...
+  // You don't want to verify on localhost
+  // try {
+  //   if (chainId !== localChainId) {
+  //     await run("verify:verify", {
+  //       address: YourContract.address,
+  //       contract: "contracts/YourContract.sol:YourContract",
+  //       constructorArguments: [],
+  //     });
+  //   }
+  // } catch (error) {
+  //   console.error(error);
+  // }
+};
+module.exports.tags = ["YourContract"];
